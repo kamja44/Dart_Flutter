86526b40eea50c42e8b4d6f64b696ec4d841218f
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-01-01 09:26:01 +0900

    변수

---
+++ Dart/변수.dart
@@ -0,0 +1,35 @@
+void main() {
+  // bool
+  bool isTrue = true;
+
+  // int
+  int num = 100;
+
+  // double
+  double num2 = 3.14;
+  double num3 = 3;
+
+  // String
+  String string = "Hello world";
+
+  // Null형 null
+  Null thisIsNull = null;
+
+  print(isTrue);
+  print(num);
+  print(num2);
+  print(string);
+  print(thisIsNull);
+
+  // 가변형 var / dynamic
+  print("==============");
+  var value = 1;
+  print(value);
+  value = 3;
+  print(value);
+
+  dynamic dynamicValue = 100;
+  print(dynamicValue);
+  dynamicValue = "Hello World";
+  print(dynamicValue);
+}
