1c233eabede52d3dda4630616b6a3c386cbe805b
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-01-02 10:25:13 +0900

    for, for in, while, do while
    반복문

--- Dart/test.dart
+++ Dart/test.dart
@@ -1,42 +1,65 @@
 void main() {
-  // bool isTrue = false;
-
-  // if
-  /*
-  if (isTrue) {
-    print("True");
-  } else if (!isTrue) {
-    print("False");
-  }
-  */
-
-  // Switch
-  /**
-   * switch (비교 대상){
-   *  case (조건식 1):
-   *  case (조건식 1):
-   *  case (조건식 1):
-   *  default :
+  /** for
+   * for (기준 변수; 조건식; 가변치){
+   * 
    * }
    * 
    */
 
-  int num = 101;
-
-  switch (num) {
-    case 1:
-      print(num);
-    case 2:
-      print(num);
-    case 3:
-      print(num);
-    case 4:
-      print(num);
-    case 5:
-      print(num);
-    case > 100:
-      print("100이상!");
-    default:
-      print("감자감자");
+  /** for 예제
+   for (int i = 0; i < 10; i++) {
+    print("Running For Index $i");
   }
+   */
+
+  /** for in
+   * for(단일 변수 in 컬렉션 (List / Set / Map)){
+   *  컬랙션 내에 요소들의 수 / 변수 만큼 사용 될 반복문
+   * }
+   * 
+   */
+
+  /** for in 예제
+  List<int> indexes = [0, 1, 2, 3, 4, 5];
+  // int 혹은 final 둘다 사용 가능 단, for에서는 final 자주 사용
+  for (final index in indexes) {
+    print("Running For Index $index");
+  }
+   */
+
+  /** while
+   * while(조건식){
+   *  조건식이 참일 경우 실행 될 반복문
+   * }
+   */
+
+  /*
+  bool isRunning = true;
+  int count = 0;
+
+  while (isRunning) {
+    if (count >= 5) {
+      isRunning = false;
+    }
+    count++;
+
+    print("While is Running $count");
+  }
+   */
+
+  /** do while
+   * do{
+   *  선행 진행 코드
+   * } while (조건);
+   */
+
+  int num = 0;
+  do {
+    num++;
+
+    if (num == 4) {
+      continue;
+    }
+    print("Running Do While $num");
+  } while (num < 10);
 }
