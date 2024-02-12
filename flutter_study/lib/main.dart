87abefca50a3e73cff5dc73cb93d6d4d51fdf764
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-02-13 08:14:50 +0900

    slider

--- flutter_study/lib/main.dart
+++ flutter_study/lib/main.dart
@@ -28,6 +28,7 @@ class Body extends StatelessWidget {
       children: [
         TestCheckBox(),
         TestRadioButton(),
+        TestSlider(),
       ],
     );
   }
@@ -119,6 +120,34 @@ class _TestRadioButtonState extends State<TestRadioButton> {
             onChanged: (value) => setState(() {
                   selectValue = value!;
                 })),
+      ],
+    );
+  }
+}
+
+class TestSlider extends StatefulWidget {
+  const TestSlider({super.key});
+
+  @override
+  State<TestSlider> createState() => _TestSliderState();
+}
+
+class _TestSliderState extends State<TestSlider> {
+  double value = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("$value"),
+        Slider(
+          value: value,
+          onChanged: (newValue) => setState(() => value = newValue),
+          divisions: 100,
+          max: 100,
+          min: 0,
+          label: value.round().toString(),
+          activeColor: Colors.red,
+        ),
       ],
     );
   }
