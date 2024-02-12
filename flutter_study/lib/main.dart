dea606b2097499902edd3fa0a71e7f5f30d7d5a2
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-02-13 08:17:42 +0900

    switch

--- flutter_study/lib/main.dart
+++ flutter_study/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,6 +30,7 @@ class Body extends StatelessWidget {
         TestCheckBox(),
         TestRadioButton(),
         TestSlider(),
+        TestSwitch(),
       ],
     );
   }
@@ -148,6 +150,30 @@ class _TestSliderState extends State<TestSlider> {
           label: value.round().toString(),
           activeColor: Colors.red,
         ),
+      ],
+    );
+  }
+}
+
+class TestSwitch extends StatefulWidget {
+  const TestSwitch({super.key});
+
+  @override
+  State<TestSwitch> createState() => _TestSwitchState();
+}
+
+class _TestSwitchState extends State<TestSwitch> {
+  bool value = false;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Switch(
+            value: value,
+            onChanged: (newValue) => setState(() => value = newValue)),
+        CupertinoSwitch(
+            value: value,
+            onChanged: (newValue) => setState(() => value = newValue)),
       ],
     );
   }
