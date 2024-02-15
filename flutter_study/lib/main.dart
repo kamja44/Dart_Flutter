817e19967e444aeb40ac9d87078139b26106b4b3
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-02-16 03:09:49 +0900

    Flutter에서 외부 라이브러리 사용하기

--- flutter_study/lib/main.dart
+++ flutter_study/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -12,9 +13,12 @@ class HomeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        body: Body(),
+        appBar: AppBar(
+          title: const Text("Flutter에서 외부 라이브러리 사용하기"),
+        ),
+        body: const Body(),
       ),
     );
   }
@@ -25,191 +29,26 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TestCheckBox(),
-        TestRadioButton(),
-        TestSlider(),
-        TestSwitch(),
-        TestPopupMenu(),
-      ],
-    );
+    return RandomWords();
   }
 }
 
-class TestCheckBox extends StatefulWidget {
-  const TestCheckBox({super.key});
-
-  @override
-  State<TestCheckBox> createState() => _TestCheckBoxState();
-}
-
-class _TestCheckBoxState extends State<TestCheckBox> {
-  late List<bool> values;
-
-  @override
-  void initState() {
-    super.initState();
-    values = [false, false, false];
-  }
+class RandomWords extends StatelessWidget {
+  const RandomWords({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Checkbox(
-            value: values[0],
-            onChanged: (value) => changeValue(0, value: value)),
-        Checkbox(
-            value: values[1],
-            onChanged: (value) => changeValue(1, value: value)),
-        Checkbox(
-            value: values[2],
-            onChanged: (value) => changeValue(2, value: value)),
-      ],
-    );
-  }
-
-  void changeValue(int index, {bool? value = false}) {
-    setState(() {
-      values[index] = value!;
-    });
-  }
-}
-
-enum TestRadioValue {
-  test1,
-  test2,
-  test3;
-}
-
-class TestRadioButton extends StatefulWidget {
-  const TestRadioButton({super.key});
-
-  @override
-  State<TestRadioButton> createState() => _TestRadioButtonState();
-}
-
-class _TestRadioButtonState extends State<TestRadioButton> {
-  TestRadioValue? selectValue;
-
-  @override
-  Widget build(BuildContext context) {
+    final wordList = generateWordPairs().take(5).toList();
+    final widgets = wordList
+        .map(
+          (word) => Text(
+            word.asCamelCase,
+            style: const TextStyle(fontSize: 32),
+          ),
+        )
+        .toList();
     return Column(
-      children: [
-        ListTile(
-          leading: Radio<TestRadioValue>(
-              value: TestRadioValue.test1,
-              groupValue: selectValue,
-              onChanged: (value) => setState(() {
-                    selectValue = value!;
-                  })),
-          title: Text(TestRadioValue.test1.name),
-          onTap: () => setState(() {
-            if (selectValue != TestRadioValue.test1) {
-              selectValue = TestRadioValue.test1;
-            }
-          }),
-        ),
-        Radio<TestRadioValue>(
-            value: TestRadioValue.test2,
-            groupValue: selectValue,
-            onChanged: (value) => setState(() {
-                  selectValue = value!;
-                })),
-        Radio<TestRadioValue>(
-            value: TestRadioValue.test3,
-            groupValue: selectValue,
-            onChanged: (value) => setState(() {
-                  selectValue = value!;
-                })),
-      ],
-    );
-  }
-}
-
-class TestSlider extends StatefulWidget {
-  const TestSlider({super.key});
-
-  @override
-  State<TestSlider> createState() => _TestSliderState();
-}
-
-class _TestSliderState extends State<TestSlider> {
-  double value = 0;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("$value"),
-        Slider(
-          value: value,
-          onChanged: (newValue) => setState(() => value = newValue),
-          divisions: 100,
-          max: 100,
-          min: 0,
-          label: value.round().toString(),
-          activeColor: Colors.red,
-        ),
-      ],
-    );
-  }
-}
-
-class TestSwitch extends StatefulWidget {
-  const TestSwitch({super.key});
-
-  @override
-  State<TestSwitch> createState() => _TestSwitchState();
-}
-
-class _TestSwitchState extends State<TestSwitch> {
-  bool value = false;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Switch(
-            value: value,
-            onChanged: (newValue) => setState(() => value = newValue)),
-        CupertinoSwitch(
-            value: value,
-            onChanged: (newValue) => setState(() => value = newValue)),
-      ],
-    );
-  }
-}
-
-class TestPopupMenu extends StatefulWidget {
-  const TestPopupMenu({super.key});
-
-  @override
-  State<TestPopupMenu> createState() => _TestPopupMenuState();
-}
-
-enum TestValue {
-  test1,
-  test2,
-  test3;
-}
-
-class _TestPopupMenuState extends State<TestPopupMenu> {
-  TestValue selectValue = TestValue.test1;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(selectValue.name),
-        PopupMenuButton(
-          itemBuilder: (context) {
-            return TestValue.values
-                .map((value) =>
-                    PopupMenuItem(value: value, child: Text(value.name)))
-                .toList();
-          },
-          onSelected: (newValue) => setState(() => selectValue = newValue),
-        ),
-      ],
+      children: widgets,
     );
   }
 }
