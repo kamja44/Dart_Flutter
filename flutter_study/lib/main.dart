eb4ac4f21d64bea40f11986e982bf7d8016a5813
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-02-16 06:21:06 +0900

    BottomNavigationBar

--- flutter_study/lib/main.dart
+++ flutter_study/lib/main.dart
@@ -9,8 +9,21 @@ void main() {
   ));
 }
 
-class HomeWidget extends StatelessWidget {
+class HomeWidget extends StatefulWidget {
   const HomeWidget({super.key});
+
+  @override
+  State<HomeWidget> createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  late int index;
+
+  @override
+  void initState() {
+    super.initState();
+    index = 0;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,9 +32,37 @@ class HomeWidget extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Assets"),
         ),
-        body: const Body(),
+        body: homeBody(),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Person"),
+          ],
+          currentIndex: index,
+          onTap: (newIndex) => setState(() {
+            index = newIndex;
+          }),
+        ),
       ),
     );
+  }
+
+  Widget homeBody() {
+    switch (index) {
+      case 1:
+        return const Center(
+          child: Icon(Icons.search, size: 100),
+        );
+      case 2:
+        return const Center(
+          child: Icon(Icons.person, size: 100),
+        );
+      default:
+        return const Center(
+          child: Icon(Icons.home, size: 100),
+        );
+    }
   }
 }
 
