6892ec4c943c05b4f3dbbc469de572bc1cc71e5c
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-02-18 01:30:37 +0900

    appbartheme, ColorScheme.light

--- flutter_study/lib/main.dart
+++ flutter_study/lib/main.dart
@@ -6,34 +6,53 @@ const assetsPath = "assets/images";
 const bannerImage = "$assetsPath/빵빵.jpg";
 
 void main() {
-  runApp(MaterialApp.router(
-    routerConfig: GoRouter(initialLocation: "/", routes: [
-      GoRoute(
-          path: "/", name: "home", builder: (context, _) => const HomeWidget()),
-      GoRoute(
-          path: "/new", name: "new", builder: (context, _) => const NewPage()),
-      GoRoute(
-          path: "/new1",
-          name: "new2",
-          builder: (context, _) => const NewPage2()),
-    ]),
-  ));
+  runApp(
+    MaterialApp(
+      home: const HomeWidget(),
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          primary: Colors.indigo,
+          secondary: Colors.green,
+          tertiary: Colors.yellow,
+        ),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.red,
+        ),
+      ),
+    ),
+  );
 }
 
-class HomeWidget extends StatelessWidget {
+class HomeWidget extends StatefulWidget {
   const HomeWidget({super.key});
 
+  @override
+  State<HomeWidget> createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  int count = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Flutter에서 화면 이동하기"),
+        title: const Text("Flutter Theme"),
       ),
       body: Center(
-        child: TextButton(
-          onPressed: () => context.pushNamed("new"),
-          child: const Text("Go to Page"),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text("Press Count"),
+            Text("$count"),
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            count++;
+          });
+        },
       ),
     );
   }
